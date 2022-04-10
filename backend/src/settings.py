d829d4d28160884ab9cbbc8a50c94c146eb11cbb
Aksarin Mikhail <m.aksarin@gmail.com>
2022-04-10 23:07:46 +0500

    Fix DATABASE_URL support

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -2,8 +2,8 @@ import os
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
-    DB_URL = DATABASE_URL
-    ASYNC_DB_URL = DB_URL.replace('postgresql', 'postgresql+asyncpg')
+    DB_URL = DATABASE_URL.replace('postgres://', 'postgresql://')
+    ASYNC_DB_URL = DATABASE_URL.replace('postgres://', 'postgresql+asyncpg://')
 else:
     DB_URL = os.getenv('DB_URL', 'postgresql://user:password@127.0.0.1:5434/woc')
     ASYNC_DB_URL = os.getenv('ASYNC_DB_URL', 'postgresql+asyncpg://user:password@127.0.0.1:5434/woc')
